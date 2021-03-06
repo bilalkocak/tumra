8d521e99e66769682f199b3b75169a92e2c256cc
bilalkocak <kocakbilal04@gmail.com>
2021-03-06 18:35:41 +0300

    Implemented redux to header and added transition for percentage bar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,32 +1,41 @@
 import React from 'react';
 import backButton from '../../assets/svg/icn-back.svg';
 import cancelButton from '../../assets/svg/Combined Shape.svg';
-
+import {setQuestionIndex} from "../../store/actions/question";
 import './Header.scss';
-import {useSelector} from "react-redux";
+import {useSelector, useDispatch} from "react-redux";
 import {useHistory} from "react-router-dom";
 
 const Header = () => {
-    const service = useSelector(state => state.service)
-    const question = useSelector(state => state.question)
-    const history = useHistory()
+        const service = useSelector(state => state.service)
+        const question = useSelector(state => state.question)
+        const history = useHistory()
+        const dispatch = useDispatch()
 
-    return (
-        <header className='app-header'>
-            <div className="header-container">
-                <div className="header-back-button" onClick={() => history.push('/')}>
-                    <img src={backButton}/>
+        const onClickBackButton = () => {
+            question.questionIndex ?
+                dispatch(setQuestionIndex(-1)) :
+                history.push('/')
+        }
+
+        return (
+            <header className='app-header'>
+                <div className="header-container">
+                    <div className="header-back-button" onClick={() => onClickBackButton()}>
+                        <img src={backButton}/>
+                    </div>
+                    <div className="header-title-container">
+                        {service.service.currentService.name}
+                    </div>
+                    <div className="header-cancel-button">
+                        <img src={cancelButton}/>
+                    </div>
                 </div>
-                <div className="header-title-container">
-                    {service.service.currentService.name}
-                </div>
-                <div className="header-cancel-button">
-                    <img src={cancelButton}/>
-                </div>
-            </div>
-            <div className="percent-bar" style={{width: `${'25'}%`}}/>
-        </header>
-    );
-};
+                <div className="percent-bar"
+                     style={{width: `${100 * (question.questionIndex) / question.questions.length}%`}}/>
+            </header>
+        );
+    }
+;
 
 export default Header;
